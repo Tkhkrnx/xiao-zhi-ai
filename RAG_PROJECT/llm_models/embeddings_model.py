3f08ab585fffc196141a7482e536c6dc23765c6b
sauhkdhaskda <sauhkdhaskda@noreply.gitcode.com>
2025-07-31 03:33:55 +0800

    Add the local embedding model to use

--- RAG_PROJECT/llm_models/embeddings_model.py
+++ RAG_PROJECT/llm_models/embeddings_model.py
@@ -1,6 +1,8 @@
+import os
 from langchain_huggingface import HuggingFaceEmbeddings
 
-model_name = "BAAI/bge-small-zh-v1.5"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_name = os.path.join(current_dir, "bge-small-zh-v1.5")
 model_kwargs = {"device": "cpu"}
 encode_kwargs = {"normalize_embeddings": True}
 bge_embedding = HuggingFaceEmbeddings(
