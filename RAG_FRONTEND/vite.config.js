79833c93fa742e6e5091baa631293e9ab278b139
sauhkdhaskda <sauhkdhaskda@noreply.gitcode.com>
2025-07-31 16:48:23 +0800

    修改配置以做nginx代理

--- RAG_FRONTEND/vite.config.js
+++ RAG_FRONTEND/vite.config.js
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/xiao-zhi-ai/',    // 关键！前端资源基础路径改成这个子路径
   server: {
+    host: true,
+    port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://150.158.55.76:8000',  // 你的后端地址和端口
+      // 这里代理时，前端开发时请求的api路径应该带 /xiao-zhi-ai/api 开头
+      '/xiao-zhi-ai/api': {
+        target: 'http://backend_xiao_zhi_ai:8001',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        rewrite: (path) => path.replace(/^\/xiao-zhi-ai\/api/, '/api'), 
       },
     },
   },
